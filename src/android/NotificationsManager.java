a5ef1475bf4a9e2d1f7869ab29b14caaaf1d1193
Geoffrey Koros <gkoros@mobileaccord.com>
2016-11-07 15:10:24 +0300

    Add initial biolerplate

---
+++ src/android/NotificationsManager.java
@@ -0,0 +1,54 @@
+package cordova-plugin-notifications-manager;
+
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * This class echoes a string called from JavaScript.
+ */
+public class NotificationsManager extends CordovaPlugin {
+
+    private static final String CHECK_OP_NO_THROW = "checkOpNoThrow";
+    private static final String OP_POST_NOTIFICATION = "OP_POST_NOTIFICATION";
+    private Context mContext = this.cordova.getActivity().getApplicationContext();
+
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        if (action.equals("isNotificationEnabled")) {
+            this.isNotificationEnabled(callbackContext);
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Check if the notifications is enabled
+     * @return
+     */
+    public void isNotificationEnabled(CallbackContext callbackContext) {
+        AppOpsManager mAppOps = (AppOpsManager) mContext.getSystemService(Context.APP_OPS_SERVICE);
+        ApplicationInfo appInfo = mContext.getApplicationInfo();
+        String pkg = mContext.getApplicationContext().getPackageName();
+        int uid = appInfo.uid;
+        Class appOpsClass = null;
+
+        try {
+            appOpsClass = Class.forName(AppOpsManager.class.getName());
+            Method checkOpNoThrowMethod = appOpsClass.getMethod(CHECK_OP_NO_THROW, Integer.TYPE, Integer.TYPE, String.class);
+            Field opPostNotificationValue = appOpsClass.getDeclaredField(OP_POST_NOTIFICATION);
+            int value = (int)opPostNotificationValue.get(Integer.class);
+            boolean enabled = ((int)checkOpNoThrowMethod.invoke(mAppOps,value, uid, pkg) == AppOpsManager.MODE_ALLOWED);
+            callbackContext.success(enabled);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            callbackContext.error(e.getMessage());
+        }
+
+        callbackContext.success(false);
+    }
+}
