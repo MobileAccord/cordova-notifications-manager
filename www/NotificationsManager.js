20ee792794587743f6c00dca89ccfbd2268cc0eb
Alex <akaikov@mobileaccord.com>
2017-01-20 10:26:08 -0700

    error issue fix

--- www/NotificationsManager.js
+++ www/NotificationsManager.js
@@ -1,5 +1,5 @@
 var exec = require('cordova/exec');
 
 window.isNotificationEnabled = function(success, error) {
-    exec(success, error, "NotificationsManager", "isNotificationEnabled", null);
+    exec(success, error, "NotificationsManager", "isNotificationEnabled", []);
 };
